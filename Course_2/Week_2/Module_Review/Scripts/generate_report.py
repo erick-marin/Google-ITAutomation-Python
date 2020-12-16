bfe4d1bf3baab831d6d10f78b30e640d69a4205c
erick-marin <emarin.iot@gmail.com>
2020-12-16 18:40:04 -0500

    Updated documentation strings.

--- Course_2/Week_2/Module_Review/Scripts/generate_report.py
+++ Course_2/Week_2/Module_Review/Scripts/generate_report.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+This script is used for course notes.
+
+Author: Erick Marin
+Date: 11/28/2020
+"""
+
 import csv
 
 
 def read_employees(csv_file_location):
+    """
+    Convert csv file to dictionary.
+
+    Receives a CSV file as a parameter and returns a list of dictionaries from
+    that file.
+    """
     csv.register_dialect("empDialect", skipinitialspace=True, strict=True)
     employee_file = csv.DictReader(
         open(csv_file_location), dialect="empDialect")
@@ -14,6 +27,12 @@ def read_employees(csv_file_location):
 
 
 def process_data(employee_list):
+    """
+    Process employee data.
+
+    Receive the list, employee_list, as a parameter and return a dictionary of
+    department:amount.
+    """
     department_list = []
     for employee_data in employee_list:
         department_list.append(employee_data['Department'])
@@ -27,6 +46,12 @@ def process_data(employee_list):
 
 
 def write_report(dictionary, report_file):
+    """
+    Generate report.
+
+    Receives dictionary as a parameter and defines an output file to generate
+    report.
+    """
     with open(report_file, "w+") as f:
         for k in sorted(dictionary):
             f.write(str(k)+":"+str(dictionary[k])+"\n")
@@ -35,7 +60,6 @@ def write_report(dictionary, report_file):
 
 employee_list = read_employees(
     "/home/k/Documents/Devs/Google-ITAutomation-Python/Course_2/Week_2/Module_Review/data/employees.csv")
-
 # print (employee_list)
 
 dictionary = process_data(employee_list)
