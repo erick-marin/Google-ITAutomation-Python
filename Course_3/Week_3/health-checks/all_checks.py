51accdd23c2cc1a055e2f6e226d15f070e2bf6bc
erick-marin <emarin.iot@gmail.com>
2021-01-11 18:35:43 -0500

    Added a simple network connectivity check

--- Course_3/Week_3/health-checks/all_checks.py
+++ Course_3/Week_3/health-checks/all_checks.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 import sys
+import socket
 
 
 def check_reboot():
@@ -27,9 +28,19 @@ def check_root_full():
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise."""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
+
 def main():
     checks = [(check_reboot, "Pending Reboot"),
-              (check_root_full, "Root partition full"), ]
+              (check_root_full, "Root partition full"),
+              (check_no_network, "No working network."), ]
 
     everything_ok = True
     for check, msg in checks:
