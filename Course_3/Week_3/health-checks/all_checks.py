d3854cb9dfc80f00c3d55d3e37bdc18063ec204e
erick-marin <emarin.iot@gmail.com>
2021-01-11 17:14:08 -0500

    Allow printing more than one error message

--- Course_3/Week_3/health-checks/all_checks.py
+++ Course_3/Week_3/health-checks/all_checks.py
@@ -30,10 +30,15 @@ def check_root_full():
 def main():
     checks = [(check_reboot, "Pending Reboot"),
               (check_root_full, "Root partition full"), ]
+
+    everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
+            everything_ok = False
+
+    if not everything_ok:
+        sys.exit(1)
 
     print("Everything ok.")
     sys.exit(0)
